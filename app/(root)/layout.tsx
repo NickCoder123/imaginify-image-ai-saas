95218c4571e15057cf232dc5c8ba3b0e97961102
Nikunj Mehra <nikunjmehra935@gmail.com>
2024-02-18 10:38:14 +0530

    Sidebar and Mobile Navigation

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,14 @@
 import { PropsWithChildren } from "react";
 
+import Sidebar from "@/components/shared/sidebar";
+import MobileNav from "@/components/shared/mobile-nav";
+
 const Layout = ({ children }: PropsWithChildren) => {
   return (
     <main className="root">
+      <Sidebar />
+      <MobileNav />
+
       <div className="root-container">
         <div className="wrapper">{children}</div>
       </div>
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,3 @@
-import { UserButton } from "@clerk/nextjs";
-
 export default function Home() {
-  return (
-    <div>
-      <p>Home</p>
-      <UserButton afterSignOutUrl="/" />
-    </div>
-  );
+  return <div></div>;
 }
